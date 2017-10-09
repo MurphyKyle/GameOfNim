2e8cf9fc21d36cbbe1c90f7d3cd15897959d4f2d
Andrew <andrewsylvester117@gmail.com>
2017-10-09 08:49:34 -0600

    Added Implementation to the player and game board class.

--- JavaCode/GameOfNim/src/model/GameBoard.java
+++ JavaCode/GameOfNim/src/model/GameBoard.java
@@ -1,25 +1,41 @@
 package model;
 
 public class GameBoard {
-	int[] rows;
-	enums.Difficulty difficulty;
+	private int[] rows;
+	private enums.Difficulty difficulty;
 	
 	public GameBoard(enums.Difficulty difficulty) {
 		this.difficulty = difficulty;
 	}
 	
 	public int getRowTokenValue(int rowNumber){
-		return 0;
+		
+		int num = rows[rowNumber];
+		
+		return num;
 	}
+	
 	public int getNumOfRows() {
-		return 0;
+		
+		int num = rows.length;
+		
+		return num;
 	}
 	public void takeTokens(int rowNumber, int numOfTokens) {
-		
+		rows[rowNumber] -= numOfTokens;
 	}
 	
 	@Override
 	public String toString() {
+		
+		for(int num : rows){
+			
+			for (int i = 0; i < num; i++) {
+				System.out.println("X");
+			}
+		}
+		
+		
 		return "";
 	}
 }
